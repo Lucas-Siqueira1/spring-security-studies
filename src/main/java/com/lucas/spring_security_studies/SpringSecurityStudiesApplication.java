8ecc2a0d15bc3cbf80f0b24b7aa4576504cf38ef
Lucas Siqueira <lucashenriquesiqueira7@gmail.com>
2026-05-07 14:43:08 -0300

    initial commit

---
+++ src/main/java/com/lucas/spring_security_studies/SpringSecurityStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.spring_security_studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/spring_security_studies/SpringSecurityStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.spring_security_studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
